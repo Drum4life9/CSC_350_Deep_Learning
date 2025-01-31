e77ca07c725a305a18aafc96e99e3a99de4d0174
Brian Myers <brianmyers691@gmail.com>
2025-01-31 10:29:35 -0500

    Perceptrons

--- 01_30_perceptrons/5.2.Implementation.py
+++ 01_30_perceptrons/5.2.Implementation.py
@@ -1,30 +1,39 @@
 import torch
 from torch import nn
 from d2l import torch as d2l
+import matplotlib.pyplot as plt
+plt.ion()
 
-class MLPScratch(d2l.Classifier):
-    def __init__(self, num_inputs, num_outputs, num_hiddens, lr, sigma=0.01):
-        super().__init__()
-        self.save_hyperparameters()
-        self.W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens) * sigma)
-        self.b1 = nn.Parameter(torch.zeros(num_hiddens))
-        self.W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs) * sigma)
-        self.b2 = nn.Parameter(torch.zeros(num_outputs))
+d2l.use_svg_display()
 
-def relu(X):
-    a = torch.zeros_like(X)
-    return torch.max(X, a)
+if __name__ == '__main__':
+    class MLPScratch(d2l.Classifier):
+        def __init__(self, num_inputs, num_outputs, num_hiddens, lr, sigma=0.01):
+            super().__init__()
+            self.save_hyperparameters()
+            self.W1 = nn.Parameter(torch.randn(num_inputs, num_hiddens) * sigma)
+            self.b1 = nn.Parameter(torch.zeros(num_hiddens))
+            self.W2 = nn.Parameter(torch.randn(num_hiddens, num_outputs) * sigma)
+            self.b2 = nn.Parameter(torch.zeros(num_outputs))
 
-@d2l.add_to_class(MLPScratch)
-def forward(self, X):
-    X = X.reshape((-1, self.num_inputs))
-    H = relu(torch.matmul(X, self.W1) + self.b1)
-    return torch.matmul(H, self.W2) + self.b2
 
-model = MLPScratch(num_inputs=784, num_outputs=10, num_hiddens=256, lr=0.1)
-data = d2l.FashionMNIST(batch_size=256)
-trainer = d2l.Trainer(max_epochs=10)
-trainer.fit(model, data)
+    def relu(X):
+        a = torch.zeros_like(X)
+        return torch.max(X, a)
+
+
+    @d2l.add_to_class(MLPScratch)
+    def forward(self, X):
+        X = X.reshape((-1, self.num_inputs))
+        H = relu(torch.matmul(X, self.W1) + self.b1)
+        return torch.matmul(H, self.W2) + self.b2
+
+
+    model = MLPScratch(num_inputs=784, num_outputs=10, num_hiddens=256, lr=0.1)
+    data = d2l.FashionMNIST(batch_size=256)
+    trainer = d2l.Trainer(max_epochs=10)
+    trainer.fit(model, data)
+    plt.show()
 
 
 class MLP(d2l.Classifier):
@@ -34,8 +43,8 @@ class MLP(d2l.Classifier):
         self.net = nn.Sequential(nn.Flatten(), nn.LazyLinear(num_hiddens),
                                  nn.ReLU(), nn.LazyLinear(num_outputs))
 
-model = MLP(num_outputs=10, num_hiddens=256, lr=0.1)
-trainer.fit(model, data)
+#model = MLP(num_outputs=10, num_hiddens=32, lr=0.1)
+#trainer.fit(model, data)
 
 
 # I'll be coming to you at some point, my programs are not running so answering
