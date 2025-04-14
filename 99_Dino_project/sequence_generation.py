a4eb19cbb55251438d5a27b2fc223aa8d9f5e1d1
Brian Myers <brianmyers691@gmail.com>
2025-04-14 13:58:46 -0400

    Almost done with dino mini project

--- 99_Dino_project/sequence_generation.py
+++ 99_Dino_project/sequence_generation.py
@@ -10,15 +10,15 @@ data = open('dinos.txt', 'r').read()
 data= data.lower()
 chars = list(set(data))
 data_size, vocab_size = len(data), len(chars)
-print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
-
-chars = sorted(chars)
-print(chars)
+# print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
+#
+# chars = sorted(chars)
+# print(chars)
 
 char_to_ix = { ch:i for i,ch in enumerate(chars) }
 ix_to_char = { i:ch for i,ch in enumerate(chars) }
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(ix_to_char)
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(ix_to_char)
 
 
 # 2.1 - Gradient Clipping
@@ -50,40 +50,40 @@ def clip(gradients, maxValue):
 
 
 # Test with a max value of 10
-def clip_test(target, mValue):
-    print(f"\nGradients for mValue={mValue}")
-    np.random.seed(3)
-    dWax = np.random.randn(5, 3) * 10
-    dWaa = np.random.randn(5, 5) * 10
-    dWya = np.random.randn(2, 5) * 10
-    db = np.random.randn(5, 1) * 10
-    dby = np.random.randn(2, 1) * 10
-    gradients = {"dWax": dWax, "dWaa": dWaa, "dWya": dWya, "db": db, "dby": dby}
-
-    gradients2 = target(gradients, mValue)
-    print("gradients[\"dWaa\"][1][2] =", gradients2["dWaa"][1][2])
-    print("gradients[\"dWax\"][3][1] =", gradients2["dWax"][3][1])
-    print("gradients[\"dWya\"][1][2] =", gradients2["dWya"][1][2])
-    print("gradients[\"db\"][4] =", gradients2["db"][4])
-    print("gradients[\"dby\"][1] =", gradients2["dby"][1])
-
-    for grad in gradients2.keys():
-        valuei = gradients[grad]
-        valuef = gradients2[grad]
-        mink = np.min(valuef)
-        maxk = np.max(valuef)
-        assert mink >= -abs(mValue), f"Problem with {grad}. Set a_min to -mValue in the np.clip call"
-        assert maxk <= abs(mValue), f"Problem with {grad}.Set a_max to mValue in the np.clip call"
-        index_not_clipped = np.logical_and(valuei <= mValue, valuei >= -mValue)
-        assert np.all(valuei[index_not_clipped] == valuef[
-            index_not_clipped]), f" Problem with {grad}. Some values that should not have changed, changed during the clipping process."
-
-    print("\033[92mAll tests passed!\x1b[0m")
-
-
-clip_test(clip, 10)
-clip_test(clip, 5)
-print("\n\n")
+# def clip_test(target, mValue):
+#     print(f"\nGradients for mValue={mValue}")
+#     np.random.seed(3)
+#     dWax = np.random.randn(5, 3) * 10
+#     dWaa = np.random.randn(5, 5) * 10
+#     dWya = np.random.randn(2, 5) * 10
+#     db = np.random.randn(5, 1) * 10
+#     dby = np.random.randn(2, 1) * 10
+#     gradients = {"dWax": dWax, "dWaa": dWaa, "dWya": dWya, "db": db, "dby": dby}
+#
+#     gradients2 = target(gradients, mValue)
+#     print("gradients[\"dWaa\"][1][2] =", gradients2["dWaa"][1][2])
+#     print("gradients[\"dWax\"][3][1] =", gradients2["dWax"][3][1])
+#     print("gradients[\"dWya\"][1][2] =", gradients2["dWya"][1][2])
+#     print("gradients[\"db\"][4] =", gradients2["db"][4])
+#     print("gradients[\"dby\"][1] =", gradients2["dby"][1])
+#
+#     for grad in gradients2.keys():
+#         valuei = gradients[grad]
+#         valuef = gradients2[grad]
+#         mink = np.min(valuef)
+#         maxk = np.max(valuef)
+#         assert mink >= -abs(mValue), f"Problem with {grad}. Set a_min to -mValue in the np.clip call"
+#         assert maxk <= abs(mValue), f"Problem with {grad}.Set a_max to mValue in the np.clip call"
+#         index_not_clipped = np.logical_and(valuei <= mValue, valuei >= -mValue)
+#         assert np.all(valuei[index_not_clipped] == valuef[
+#             index_not_clipped]), f" Problem with {grad}. Some values that should not have changed, changed during the clipping process."
+#
+#     print("\033[92mAll tests passed!\x1b[0m")
+#
+#
+# clip_test(clip, 10)
+# clip_test(clip, 5)
+# print("\n\n")
 
 
 # TODO - Exercise 2 - sample
@@ -163,80 +163,80 @@ def sample(parameters, char_to_ix, seed):
     return indices
 
 
-def sample_test(target):
-    np.random.seed(24)
-    _, n_a = 20, 100
-    Wax, Waa, Wya = np.random.randn(n_a, vocab_size), np.random.randn(n_a, n_a), np.random.randn(vocab_size, n_a)
-    b, by = np.random.randn(n_a, 1), np.random.randn(vocab_size, 1)
-    parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b, "by": by}
+# def sample_test(target):
+#     np.random.seed(24)
+#     _, n_a = 20, 100
+#     Wax, Waa, Wya = np.random.randn(n_a, vocab_size), np.random.randn(n_a, n_a), np.random.randn(vocab_size, n_a)
+#     b, by = np.random.randn(n_a, 1), np.random.randn(vocab_size, 1)
+#     parameters = {"Wax": Wax, "Waa": Waa, "Wya": Wya, "b": b, "by": by}
+#
+#     indices = target(parameters, char_to_ix, 0)
+#     print("Sampling:")
+#     print("list of sampled indices:\n", indices)
+#     print("list of sampled characters:\n", [ix_to_char[i] for i in indices])
+#
+#     assert len(indices) < 52, "Indices length must be smaller than 52"
+#     assert indices[-1] == char_to_ix['\n'], "All samples must end with \\n"
+#     assert min(indices) >= 0 and max(indices) < len(
+#         char_to_ix), f"Sampled indexes must be between 0 and len(char_to_ix)={len(char_to_ix)}"
+#     assert np.allclose(indices,
+#                        [23, 16, 26, 26, 24, 3, 21, 1, 7, 24, 15, 3, 25, 20, 6, 13, 10, 8, 20, 12, 2, 0]), "Wrong values"
+#
+#     print("\033[92mAll tests passed!\x1b[0m")
+#
+#
+# sample_test(sample)
+# print("\n\n")
 
-    indices = target(parameters, char_to_ix, 0)
-    print("Sampling:")
-    print("list of sampled indices:\n", indices)
-    print("list of sampled characters:\n", [ix_to_char[i] for i in indices])
 
-    assert len(indices) < 52, "Indices length must be smaller than 52"
-    assert indices[-1] == char_to_ix['\n'], "All samples must end with \\n"
-    assert min(indices) >= 0 and max(indices) < len(
-        char_to_ix), f"Sampled indexes must be between 0 and len(char_to_ix)={len(char_to_ix)}"
-    assert np.allclose(indices,
-                       [23, 16, 26, 26, 24, 3, 21, 1, 7, 24, 15, 3, 25, 20, 6, 13, 10, 8, 20, 12, 2, 0]), "Wrong values"
+# 3.1 - Gradient Descent
+# TODO - Exercise 3 - optimize function
+def optimize(X, Y, a_prev, parameters, learning_rate=0.01):
+    """
+    Execute one step of the optimization to train the model.
 
-    print("\033[92mAll tests passed!\x1b[0m")
+    Arguments:
+    X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
+    Y -- list of integers, exactly the same as X but shifted one index to the left.
+    a_prev -- previous hidden state.
+    parameters -- python dictionary containing:
+                        Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
+                        Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
+                        Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
+                        b --  Bias, numpy array of shape (n_a, 1)
+                        by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
+    learning_rate -- learning rate for the model.
+
+    Returns:
+    loss -- value of the loss function (cross-entropy)
+    gradients -- python dictionary containing:
+                        dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
+                        dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
+                        dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
+                        db -- Gradients of bias vector, of shape (n_a, 1)
+                        dby -- Gradients of output bias vector, of shape (n_y, 1)
+    a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
+    """
+
+    ### TODO - START CODE HERE ###
+
+    # Forward propagate through time
+    loss, cache = rnn_forward(X, Y, a_prev, parameters)
+
+    # Backpropagate through time
+    gradients, a = rnn_backward(X, Y, parameters, cache)
+
+    # Clip your gradients between -5 (min) and 5 (max)
+    gradients = dict([k, np.clip(v, -5, 5)] for k,v in gradients.items())
+
+    # Update parameters
+    parameters = update_parameters(parameters, gradients, learning_rate)
+
+    ### END CODE HERE ###
+
+    return loss, gradients, a[len(X) - 1]
 
 
-sample_test(sample)
-print("\n\n")
-
-
-# # 3.1 - Gradient Descent
-# # TODO - Exercise 3 - optimize function
-# def optimize(X, Y, a_prev, parameters, learning_rate=0.01):
-#     """
-#     Execute one step of the optimization to train the model.
-#
-#     Arguments:
-#     X -- list of integers, where each integer is a number that maps to a character in the vocabulary.
-#     Y -- list of integers, exactly the same as X but shifted one index to the left.
-#     a_prev -- previous hidden state.
-#     parameters -- python dictionary containing:
-#                         Wax -- Weight matrix multiplying the input, numpy array of shape (n_a, n_x)
-#                         Waa -- Weight matrix multiplying the hidden state, numpy array of shape (n_a, n_a)
-#                         Wya -- Weight matrix relating the hidden-state to the output, numpy array of shape (n_y, n_a)
-#                         b --  Bias, numpy array of shape (n_a, 1)
-#                         by -- Bias relating the hidden-state to the output, numpy array of shape (n_y, 1)
-#     learning_rate -- learning rate for the model.
-#
-#     Returns:
-#     loss -- value of the loss function (cross-entropy)
-#     gradients -- python dictionary containing:
-#                         dWax -- Gradients of input-to-hidden weights, of shape (n_a, n_x)
-#                         dWaa -- Gradients of hidden-to-hidden weights, of shape (n_a, n_a)
-#                         dWya -- Gradients of hidden-to-output weights, of shape (n_y, n_a)
-#                         db -- Gradients of bias vector, of shape (n_a, 1)
-#                         dby -- Gradients of output bias vector, of shape (n_y, 1)
-#     a[len(X)-1] -- the last hidden state, of shape (n_a, 1)
-#     """
-#
-#     ### TODO - START CODE HERE ###
-#
-#     # Forward propagate through time
-#     loss, cache =
-#
-#     # Backpropagate through time
-#     gradients, a =
-#
-#     # Clip your gradients between -5 (min) and 5 (max)
-#     gradients =
-#
-#     # Update parameters
-#     parameters =
-#
-#     ### END CODE HERE ###
-#
-#     return loss, gradients, a[len(X) - 1]
-#
-#
 # def optimize_test(target):
 #     np.random.seed(1)
 #     vocab_size, n_a = 27, 100
@@ -271,101 +271,102 @@ print("\n\n")
 # print("\n\n")
 #
 #
-# # 3.2 - Training the Model
-# # TODO - Exercise 4 - model
-# def model(data_x, ix_to_char, char_to_ix, num_iterations=35000, n_a=50, dino_names=7, vocab_size=27, verbose=False):
-#     """
-#     Trains the model and generates dinosaur names.
-#
-#     Arguments:
-#     data_x -- text corpus, divided in words
-#     ix_to_char -- dictionary that maps the index to a character
-#     char_to_ix -- dictionary that maps a character to an index
-#     num_iterations -- number of iterations to train the model for
-#     n_a -- number of units of the RNN cell
-#     dino_names -- number of dinosaur names you want to sample at each iteration.
-#     vocab_size -- number of unique characters found in the text (size of the vocabulary)
-#
-#     Returns:
-#     parameters -- learned parameters
-#     """
-#
-#     # Retrieve n_x and n_y from vocab_size
-#     n_x, n_y = vocab_size, vocab_size
-#
-#     # Initialize parameters
-#     parameters = initialize_parameters(n_a, n_x, n_y)
-#
-#     # Initialize loss (this is required because we want to smooth our loss)
-#     loss = get_initial_loss(vocab_size, dino_names)
-#
-#     # Build list of all dinosaur names (training examples).
-#     examples = [x.strip() for x in data_x]
-#
-#     # Shuffle list of all dinosaur names
-#     np.random.seed(0)
-#     np.random.shuffle(examples)
-#
-#     # Initialize the hidden state of your LSTM
-#     a_prev = np.zeros((n_a, 1))
-#
-#     # for testing purposes
-#     last_dino_name = "abc"
-#
-#     # Optimization loop
-#     for j in range(num_iterations):
-#
-#         ### TODO - START CODE HERE ###
-#
-#         # Set the index `idx`
-#         idx =
-#
-#         # Set the input X
-#         single_example =
-#         single_example_chars =
-#         single_example_ix =
-#         X =
-#
-#         # Set the labels Y (see instructions above)
-#         Y = X[1:] + [char_to_ix["\n"]]
-#
-#         # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
-#         # Choose a learning rate of 0.01
-#         curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters)
-#
-#         ### END CODE HERE ###
-#
-#         # debug statements to aid in correctly forming X, Y
-#         if verbose and j in [0, len(examples) - 1, len(examples)]:
-#             print("j = ", j, "idx = ", idx, )
-#         if verbose and j in [0]:
-#             print("single_example =", single_example)
-#             print("single_example_chars", single_example_chars)
-#             print("single_example_ix", single_example_ix)
-#             print(" X = ", X, "\n", "Y =       ", Y, "\n")
-#
-#         # to keep the loss smooth.
-#         loss = smooth(loss, curr_loss)
-#
-#         # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
-#         if j % 2000 == 0:
-#
-#             print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
-#
-#             # The number of dinosaur names to print
-#             seed = 0
-#             for name in range(dino_names):
-#                 # Sample indices and print them
-#                 sampled_indices = sample(parameters, char_to_ix, seed)
-#                 last_dino_name = get_sample(sampled_indices, ix_to_char)
-#                 print(last_dino_name.replace('\n', ''))
-#
-#                 seed += 1  # To get the same result (for testing purposes), increment the seed by one.
-#
-#             print('\n')
-#
-#     return parameters, last_dino_name
-#
-# parameters, last_name = model(data.split("\n"), ix_to_char, char_to_ix, 22001, verbose = True)
-# print(f"Final name: {last_name}")
+# 3.2 - Training the Model
+# TODO - Exercise 4 - model
+def model(data_x, ix_to_char, char_to_ix, num_iterations=35000, n_a=50, dino_names=7, vocab_size=27, verbose=False):
+    """
+    Trains the model and generates dinosaur names.
+
+    Arguments:
+    data_x -- text corpus, divided in words
+    ix_to_char -- dictionary that maps the index to a character
+    char_to_ix -- dictionary that maps a character to an index
+    num_iterations -- number of iterations to train the model for
+    n_a -- number of units of the RNN cell
+    dino_names -- number of dinosaur names you want to sample at each iteration.
+    vocab_size -- number of unique characters found in the text (size of the vocabulary)
+
+    Returns:
+    parameters -- learned parameters
+    """
+
+    # Retrieve n_x and n_y from vocab_size
+    n_x, n_y = vocab_size, vocab_size
+
+    # Initialize parameters
+    parameters = initialize_parameters(n_a, n_x, n_y)
+
+    # Initialize loss (this is required because we want to smooth our loss)
+    loss = get_initial_loss(vocab_size, dino_names)
+
+    # Build list of all dinosaur names (training examples).
+    examples = [x.strip() for x in data_x]
+
+    # Shuffle list of all dinosaur names
+    np.random.seed(0)
+    np.random.shuffle(examples)
+
+    # Initialize the hidden state of your LSTM
+    a_prev = np.zeros((n_a, 1))
+
+    # for testing purposes
+    last_dino_name = "abc"
+
+    # Optimization loop
+    for j in range(num_iterations):
+
+        ### TODO - START CODE HERE ###
+
+        # Set the index `idx`
+        idx = j
+
+        # Set the input X
+        single_example = examples[idx]
+        single_example_chars = [c for c in single_example]
+        single_example_ix = [char_to_ix[c] for c in single_example_chars]
+        # single_example_ix.insert(1, None)
+        X = rnn_forward(single_example_ix, single_example_chars, a_prev, parameters)
+
+        # Set the labels Y (see instructions above)
+        Y = X[1:] + [char_to_ix["\n"]]
+
+        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
+        # Choose a learning rate of 0.01
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters)
+
+        ### END CODE HERE ###
+
+        # debug statements to aid in correctly forming X, Y
+        if verbose and j in [0, len(examples) - 1, len(examples)]:
+            print("j = ", j, "idx = ", idx, )
+        if verbose and j in [0]:
+            print("single_example =", single_example)
+            print("single_example_chars", single_example_chars)
+            print("single_example_ix", single_example_ix)
+            print(" X = ", X, "\n", "Y =       ", Y, "\n")
+
+        # to keep the loss smooth.
+        loss = smooth(loss, curr_loss)
+
+        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
+        if j % 2000 == 0:
+
+            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
+
+            # The number of dinosaur names to print
+            seed = 0
+            for name in range(dino_names):
+                # Sample indices and print them
+                sampled_indices = sample(parameters, char_to_ix, seed)
+                last_dino_name = get_sample(sampled_indices, ix_to_char)
+                print(last_dino_name.replace('\n', ''))
+
+                seed += 1  # To get the same result (for testing purposes), increment the seed by one.
+
+            print('\n')
+
+    return parameters, last_dino_name
+
+parameters, last_name = model(data.split("\n"), ix_to_char, char_to_ix, 22001, verbose = True)
+print(f"Final name: {last_name}")
 
